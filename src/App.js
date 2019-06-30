77e2c711dc2b9d76209ab28141b0af0411b20e58
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-06-30 18:19:25 +0100

    some CRA remnants removed and functions outlined with comments as instructions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      currentPlayer: 0,
+      turnScore: 0,
+      gameStarted: false,
+      gameWon: false
+    };
+
+    // Bind this to each function instance
+    this.addPlayer = this.addPlayer.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.changeTurn = this.changeTurn.bind(this);
+    this.rollDice = this.rollDice.bind(this);
+    this.endTurn = this.endTurn.bind(this);
+    this.isGameWon = this.isGameWon.bind(this);
+  }
+
+  addPlayer = () => {
+    // Add Object to players array in state
+    // Spread in current players array and add new player
+    // Player Obj has name & overall score
+    // Name should count how many items in array and append 'Player '
+  }
+
+  startGame = () => {
+    // Flip 'gameStarted' bool to true
+  }
+
+  changeTurn = () => {
+    // Detect whether it's the final player
+    // If so, change currentPlayer back to 0
+    // If not, currentPlayer++
+  }
+
+  rollDice = () => {
+    // Set 2x random variable from 1-6
+    // Detect whether both dice are a 1
+    // If so, set player score to 0 and endTurn()
+    // Detect whether either dice are a 1
+    // if so, endTurn()
+    // else, add dice total to turnScore
+  }
+
+  endTurn = () => {
+    // Add turnScore to player score
+    // set turnScore back to 0
+    // isGameWon()
+    // changeTurn()
+  }
+
+  isGameWon = () => {
+    // Check whether current player score is 100+
+    // If it is, flip gameWon bool to true
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Pigs!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
